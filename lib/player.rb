68b6899ce82b7257c6439b8ffa17501e758b54ad
Katelyn <katelyn.rimkunas@gmail.com>
2018-09-04 23:48:16 -0600

    validate ship placement

--- lib/player.rb
+++ lib/player.rb
@@ -13,7 +13,30 @@ class Player
   end
 
 
+  # --- Ship Placement ---
+
+  def process_coordinates(string)
+    [ string[0..1], string[3..4] ]
+  end
+
+
+
+
+
+
+
+
   # --- Ship Placement Validation ---
+
+  def validate_ship_placement(string, size)
+    pick_random if string == "random"
+    form = assess_format(string)
+    grid = assess_diagonal(string)
+    space = assess_spacing(string, size)
+    possible = assess_as_possible(string)
+    return true if form && grid && space && possible
+    # repromt
+  end
 
   def assess_format(string)
     case1 = string.size == 5
@@ -26,21 +49,43 @@ class Player
     p "Check your format!\nsize 2: A1 A2\nsize 3: A1 A3"
     return false
   end
-  # TO DO -- if 2nd position is "less than" first
-  # is there a fuzzy match method?
-  # if so, this doesn't matter
 
-  # def assess_diagonal(input) # array
-  #
-  #   #  create array
-  #   # assess
-  #   # if valid = false
-  #     #  print error message
-  #   p "You can't place a boat diagonally."
-  # end
+  def assess_diagonal(string)
+    arr = process_coordinates(string)
+    row_1 = arr[0][0]; column_1 = arr[0][1]
+    row_2 = arr[1][0]; column_2 = arr[1][1]
+    case1 = row_1 == row_2
+    case2 = column_1 == column_2
+    return true if case1 || case2
+    p "You can't place a boat diagonally."
+    return false
+  end
+  # THESE TWO METHODS CAN BE COMBINED (diagonal & spacing)
+  def assess_spacing(string, size)
+    arr = process_coordinates(string).join.chars
+    columns = (arr[1]..arr[3]).to_a.size
+    rows = (arr[0]..arr[2]).to_a.size
+    case1 = columns == size && rows == 1
+    case2 = rows == size && columns == 1
+    return true if case1 || case2
+    p "You're placing a boat with #{size} anchors"
+    return false
+  end
+
+# === COME BACK TO =====================================
+  def assess_as_possible(string)
+    arr = process_coordinates(string)
+    possible = @board.possible_positions
+    possible.find {|set| set.first == arr.first && set.last == arr.last}
+  end
+# === make board method =====================================
+# TAKE CARE OF OVERLAP VALIDATION via possible_positions
+
 
 
 end
+
+
   # def place_ships(sizes = [2, 3])
   #   sizes.each { |size| place_ship(size) }
   # end
@@ -73,17 +118,7 @@ end
 
   # --- Ship Placement Validation ---
 
-  # def validate_ship_placement(string, choices, size)
-  #   pick_random(choices) if string == "random"
-  #   form = assess_format(string)
-  #   # change format of input to array
-  #   arr = process_coordinates(string)
-  #   grid = assess_diagonal(arr)
-  #   space = assess_spacing(arr, size)
-  #   # =====================================
-  #   # unique = assess_overlap()
-  #   # =====================================
-  #   form && grid && space && unique ? true : nil # repromt
+  # def
   # end
 
 
@@ -93,12 +128,7 @@ end
 
 
 
-  # def assess_spacing(input, size) # array & ship size
-  #   # assess
-  #   # if valid = false
-  #     #  print error message
-  #   p "You're placing a boat with #{size} anchors"
-  # end
+
 
 
   # --- Shooting validation ---
--- test/player_test.rb
+++ test/player_test.rb
@@ -21,7 +21,28 @@ class PlayerTest < Minitest::Test
   end
 
 
+  # --- Ship Placement ---
+
+  def test_it_can_process_input_coordinates
+    human = Player.new(true)
+    arr = human.process_coordinates("A1 A2")
+    assert_equal ["A1", "A2"], arr
+  end
+
+
+
   # --- Ship Placement Validation ---
+
+  def test_it_can_validate_ship_placement
+    skip
+    human = Player.new(true)
+    valid = "A1 B1"
+    invalid = "A1 B2"
+
+    assert_equal true, human.validate_ship_placement(valid, 2)
+    assert_nil human.validate_ship_placement(invalid, 2)
+  end
+
 
   def test_it_can_assess_format_of_ship_placement
     human = Player.new(true)
@@ -32,7 +53,7 @@ class PlayerTest < Minitest::Test
     invalid_size = [" ", "A1", "A1A2", " A1 A2 "]
     invalid_form = ["A1A2 ", " A1A2",]
     invalid_content = ["AAAAA", "11111", "A1234", "123A1"]
-    
+
     all_valid = valid.all? { |input| human.assess_format(input) }
     assert_equal true, all_valid
 
@@ -46,6 +67,42 @@ class PlayerTest < Minitest::Test
 
     all_invalid_content = invalid_content.all? { |input| human.assess_format(input) == false }
     assert_equal true, all_invalid_content
+  end
+
+  def test_it_can_assess_diagonal_placement
+    human = Player.new(true)
+    valid = "A1 B1"
+    invalid = "A1 B2"
+    assert_equal true, human.assess_diagonal(valid)
+    assert_equal false, human.assess_diagonal(invalid)
+  end
+
+  def test_it_can_assess_space_between_coordinates
+    human = Player.new(true)
+    valid_1 = "A1 B1"
+    valid_2 = "B1 B2"
+    invalid_1 = "A1 A3"
+    invalid_2 = "A1 A1" # demonstrates if same coordinate is entered twice
+    invalid_3 = "A1 C1"
+    invalid_4 = "A2 A1" # demonstrates if order is incorrect
+
+    assert_equal true, human.assess_spacing(valid_1, 2)
+    assert_equal true, human.assess_spacing(valid_2, 2)
+    assert_equal true, human.assess_spacing(valid_2, 2)
+    assert_equal false, human.assess_spacing(invalid_1, 2)
+    assert_equal false, human.assess_spacing(invalid_2, 2)
+    assert_equal false, human.assess_spacing(invalid_3, 2)
+    assert_equal false, human.assess_spacing(invalid_4, 2)
+  end
+
+  def test_it_can_assess_coordinate_set_as_possible
+    skip
+    human = Player.new(true)
+    valid_1 = "A1 B1"
+    invalid_1 = "A1 A3"
+
+    assert_equal true,  human.assess_possible(valid_1)
+    assert_equal false, human.assess_possible(invalid_1)
   end
 
 
